bd14ed7a4d43f2d2517b2f4f0741129b28925aa3
RafaelDeve <rafaelaybarp@gmail.com>
2025-08-22 19:20:05 -0400

    Cambios

--- src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acafddd934922de1ae348940730a97a613bcf189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb9d6bc279b8105ef5dcdb7349959afa5a0326c")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
