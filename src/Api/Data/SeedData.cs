2212021ed55da7037218827ac3c2a9924811a17a
Luis Elier Pujols Rosa <luiselierpujolsrosa@gmail.com>
2025-08-21 21:07:00 -0400

    correcion-api

--- src/Api/Data/SeedData.cs
+++ src/Api/Data/SeedData.cs
@@ -1,49 +1,53 @@
+namespace Api.Data;
 
 using Api.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace Api.Data;
-public static class SeedData {
-    public static async Task EnsureSeedAsync(AppDbContext db) {
-        if (await db.Users.AnyAsync()) return;
+public static class DatosSemilla {
+    public static async Task AsegurarSemillaAsync(ContextoAplicacion db) {
+        if (await db.Usuarios.AnyAsync()) return;
 
-        var genres = new[] { "Action", "Drama", "Comedy", "Sci-Fi", "Romance", "Thriller" }
-            .Select((n,i) => new Genre { Id = i+1, Nombre = n }).ToList();
-        db.Genres.AddRange(genres);
+        var generos = new[] { "Acción", "Drama", "Comedia", "Ciencia Ficción", "Romance", "Suspenso" }
+            .Select((n,i) => new Genero { Id = i+1, Nombre = n }).ToList();
+        db.Generos.AddRange(generos);
 
-        var rnd = new Random(42);
-        var movies = Enumerable.Range(1, 500)
-            .Select(i => new Movie { Id = i, Titulo = $"Movie {i}" })
-            .ToList();
-        db.Movies.AddRange(movies);
+        var aleatorio = new Random(42);
+        var peliculas = Enumerable.Range(1, 500)
+            .Select(i => new Pelicula { Id = i, Titulo = $"Pelicula {i}" }).ToList();
+        db.Peliculas.AddRange(peliculas);
 
-        var mgs = new List<GeneroPelicula>();
-        foreach (var m in movies) {
-            var gcount = rnd.Next(1, 4);
-            var picked = genres.OrderBy(_ => rnd.Next()).Take(gcount).ToList();
-            picked.ForEach(g => mgs.Add(new GeneroPelicula { IdPelicula = m.Id, IdGenero = g.Id }));
+        var enlacesPG = new List<PeliculaGenero>();
+        foreach (var p in peliculas) {
+            var cantidadG = aleatorio.Next(1, 4);
+            var tomados = generos.OrderBy(_ => aleatorio.Next()).Take(cantidadG).ToList();
+            tomados.ForEach(g => enlacesPG.Add(new PeliculaGenero { PeliculaId = p.Id, GeneroId = g.Id }));
         }
-        db.GenerosPelicula.AddRange(mgs);
+        db.PeliculaGeneros.AddRange(enlacesPG);
 
-        var users = Enumerable.Range(1, 5_000)
-            .Select(i => new User { Id = i, Email = $"user{i}@demo.local" }).ToList();
-        db.Users.AddRange(users);
+        var usuarios = Enumerable.Range(1, 5000)
+            .Select(i => new Usuario { Id = i, Correo = $"usuario{i}@demo.local" }).ToList();
+        db.Usuarios.AddRange(usuarios);
 
-        var ratings = new List<Rating>();
-        foreach (var u in users) {
-            var rated = movies.OrderBy(_ => rnd.Next()).Take(rnd.Next(30, 120)).ToList();
-            rated.ForEach(m => ratings.Add(new Rating {
-                IdUsuario = u.Id, IdPelicula = m.Id, Score = (float)(rnd.Next(1, 10) / 2.0),
-                Timestamp = DateTime.UtcNow.AddDays(-rnd.Next(1, 365))
+        var calificaciones = new List<Calificacion>();
+        foreach (var u in usuarios) {
+            var vistas = peliculas.OrderBy(_ => aleatorio.Next()).Take(aleatorio.Next(30, 120)).ToList();
+            vistas.ForEach(p => calificaciones.Add(new Calificacion {
+                UsuarioId = u.Id,
+                PeliculaId = p.Id,
+                Puntuacion = (float)(aleatorio.Next(1, 10) / 2.0),
+                Momento = DateTime.UtcNow.AddDays(-aleatorio.Next(1, 365))
             }));
         }
-        db.Ratings.AddRange(ratings);
+        db.Calificaciones.AddRange(calificaciones);
 
-        var trends = movies.Take(50).Select(m => new Trend {
-            IdPelicula = m.Id, GlobalHotness = (float)rnd.NextDouble() * 2f, UpdatedAt = DateTime.UtcNow
+        var tendencias = peliculas.Take(50).Select(p => new Tendencia {
+            PeliculaId = p.Id,
+            IntensidadGlobal = (float)aleatorio.NextDouble() * 2f,
+            ActualizadoEn = DateTime.UtcNow
         }).ToList();
-        db.Trends.AddRange(trends);
+        db.Tendencias.AddRange(tendencias);
 
         await db.SaveChangesAsync();
     }
 }
+
